f1a340f36419a2f2928444ceb973a7cbbf837a5d
Origami-Dragon-Studios <75270387+Origami-Dragon-Studios@users.noreply.github.com>
2021-03-12 20:29:06 -0800

    Beginnings of state machine

--- Game Jam Game/scripts/player.gd
+++ Game Jam Game/scripts/player.gd
@@ -8,30 +8,47 @@ export var movement_speed := 100
 export var gravity := 10
 export var jump_height = 400
 
-var velocity := Vector2()
+enum states {REGULAR, NO_GRAVITY}
 
 # for messing with the inputs
 var input_map = {"right": 1,
 				 "left": -1,
 				 "up": -1}
 
+# go ahead and guess what this is
+var velocity := Vector2()
+
+# state machine
+var state = states.REGULAR
+
 func _physics_process(delta):
-	# gravity
-	velocity.y += gravity
+	# resetting velocity
+	if is_on_ceiling():
+		velocity.y = 0
+	if is_on_floor(): 
+		velocity.y = 0
 	
-	# movement
+	# left and right movement
 	if Input.is_action_pressed("ui_right"):
 		velocity.x = input_map.right * movement_speed
 	elif Input.is_action_pressed("ui_left"):
 		velocity.x = input_map.left * movement_speed
-	else:
-		velocity.x = 0
 	
-	# jumping
-	if is_on_floor(): 
-		velocity.y = 0
-		if Input.is_action_pressed("ui_up"):
-			velocity.y = input_map.up * jump_height
-	
+	# STATE MACHINEEEEE YOOOOOOOOOOOOOOOOOOOOOOOOOO
+	match state:
+		states.REGULAR:
+			# gravity
+			velocity.y += gravity
+			
+			# resetting movement
+			if !(Input.is_action_pressed("ui_right") or Input.is_action_pressed("ui_left")):
+				velocity.x = 0
+			
+			# jumping
+			if Input.is_action_pressed("ui_up"):
+				if is_on_floor():
+						velocity.y = input_map.up * jump_height
+		# blowdryer code
+#		states.NO_GRAVITY:
 	# changing position
 	move_and_slide(velocity, UP)
