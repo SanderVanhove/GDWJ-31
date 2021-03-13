247307b9b06b9a63130c17531a56216faf96e2e0
Origami-Dragon-Studios <75270387+Origami-Dragon-Studios@users.noreply.github.com>
2021-03-13 01:28:19 -0800

    c a c t u s
    c a c
    
    t      u s

---
+++ Game Jam Game/scripts/cactus.gd
@@ -0,0 +1,9 @@
+extends StaticBody2D
+
+signal blow_back(pos)
+
+
+func _on_blow_back_area_body_entered(body):
+	connect("blow_back", body, "_cactus_blow_back")
+	emit_signal("blow_back", position)
+	disconnect("blow_back", body, "_cactus_blow_back")
--- Game Jam Game/scripts/player.gd
+++ Game Jam Game/scripts/player.gd
@@ -4,9 +4,10 @@ extends KinematicBody2D
 const UP = Vector2(0, -1)
 
 # exports
-export var movement_speed := 100
+export var movement_speed := 1
 export var gravity := 10
-export var jump_height = 400
+export var jump_height := 400
+export var max_speed := 200
 
 enum states {REGULAR, NO_GRAVITY}
 
@@ -17,6 +18,8 @@ var input_map = {"right": 1,
 
 # go ahead and guess what this is
 var velocity := Vector2()
+
+var velocity_2 := Vector2()
 
 # starting position
 var starting_position = Vector2()
@@ -29,16 +32,12 @@ func _ready():
 
 func _physics_process(delta):
 	# resetting velocity
-	if is_on_ceiling():
-		velocity.y = 0
-	if is_on_floor(): 
-		velocity.y = 0
 	
 	# left and right movement
 	if Input.is_action_pressed("ui_right"):
-		velocity.x = input_map.right * movement_speed
+		velocity.x += input_map.right * movement_speed
 	elif Input.is_action_pressed("ui_left"):
-		velocity.x = input_map.left * movement_speed
+		velocity.x += input_map.left * movement_speed
 	
 	# STATE MACHINEEEEE YOOOOOOOOOOOOOOOOOOOOOOOOOO
 	match state:
@@ -46,21 +45,21 @@ func _physics_process(delta):
 			# gravity
 			velocity.y += gravity
 			
-			# resetting movement
-			if !(Input.is_action_pressed("ui_right") or Input.is_action_pressed("ui_left")):
-				velocity.x = 0
-			
 			# jumping
 			if Input.is_action_pressed("ui_up"):
 				if is_on_floor():
-						velocity.y = input_map.up * jump_height
+						velocity.y += input_map.up * jump_height
 		# blowdryer code
 #		states.NO_GRAVITY:
 		
 	# changing position
-	move_and_slide(velocity, UP)
+	velocity.x = clamp(velocity.x, -max_speed, max_speed)
+	velocity = move_and_slide(velocity, UP)
 
 func _die():
 	velocity.y = 0
 	# death animation/particles or whatever
 	position = starting_position
+
+func _cactus_blow_back(pos):
+	velocity += Vector2(sign(position.x - pos.x) * 300, -400)
